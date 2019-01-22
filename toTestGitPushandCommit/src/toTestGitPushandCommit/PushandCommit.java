f64fb6f0294a9e3a4cb0efc0a962c44a8ae11bab
amikusin <amikusin@AMIKUSIN-IN.oradev.oraclecorp.com>
2019-01-22 12:24:23 +0530

    heloo Amit first commit

---
+++ toTestGitPushandCommit/src/toTestGitPushandCommit/PushandCommit.java
@@ -0,0 +1,10 @@
+package toTestGitPushandCommit;
+
+public class PushandCommit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi Vinay");
+	}
+
+}
